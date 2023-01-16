15d310a9e8f92aa0e4039d882512a7e7e1ae167f
jousef1 <yousefel560@gmail.com>
2023-01-16 09:10:37 +0800

    setup the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
     <div className="gpt3__navbar-links">
@@ -23,7 +25,7 @@ const Navbar = () => {
       <p>Sign in</p>
       <button type="button">Sign up</button>
     </div>
-    {/* <div className="gpt3__navbar-menu">
+    <div className="gpt3__navbar-menu">
       {toggleMenu
         ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
         : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
@@ -42,7 +44,7 @@ const Navbar = () => {
         </div>
       </div>
       )}
-    </div> */}
+    </div>
   </div>
   )
 }
